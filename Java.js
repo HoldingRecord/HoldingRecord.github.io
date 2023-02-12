09fe0355d2f1dd4e9daa060bf7bb332526410e44
Hold <91856885+HoldingRecord@users.noreply.github.com>
2023-02-12 12:53:50 -0600

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,1 +1,8 @@
-console.log("Hello")
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+  }, false);
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "F12") {
+      console.log("Developer tools detected!");
+    }
+  });
